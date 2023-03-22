8c4cde9681d4de5e8d41d19f5939b7810356fcc2
ragazzibonjour <ragazzi.bonjour@gmail.com>
2023-03-22 10:29:05 -0700

    Initial Commit

---
+++ DemoGitUITests/DemoGitUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  DemoGitUITestsLaunchTests.swift
+//  DemoGitUITests
+//
+//  Created by Mercedes Martinez Milantchi on 3/22/23.
+//
+
+import XCTest
+
+class DemoGitUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
